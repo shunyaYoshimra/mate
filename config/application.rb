fe2e8930e74cccb861771545afc2f00176b63edc
shunyaYoshimura <maoorgri1015@gmail.com>
2020-09-12 16:16:49 +0900

    make home controller

--- config/application.rb
+++ config/application.rb
@@ -11,18 +11,18 @@ module Mate
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.test_framework :rspec, 
+            view_specs: false, 
+            helper_specs: false, 
+            controller_specs: false, 
+            routing_specs: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 
-  config.generators do |g|
-    g.test_framework :rspec, 
-          view_specs: false, 
-          helper_specs: false, 
-          controller_specs: false, 
-          routing_specs: false
-  end
-  
 end
