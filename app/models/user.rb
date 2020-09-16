67a00f3750520b6f740446e9d6d34c1506da48e0
shunyaYoshimura <maoorgri1015@gmail.com>
2020-09-16 12:54:33 +0900

    create relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :events, dependent: :destroy
   has_many :participants, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name:  'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :active_relationships
+  has_many :followers, through: :passive_relationships
+
+  def following?(other_user)
+    active_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    active_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(following_id: other_user.id).destroy
+  end
 end
