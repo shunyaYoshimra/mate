6804e980b934dc9fd164f6e996043a29b4c1d8a4
shunyaYoshimura <maoorgri1015@gmail.com>
2020-09-16 13:23:56 +0900

    create follow function

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -33,6 +33,18 @@ class ProfilesController < ApplicationController
     @profile.destroy
   end
 
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end 
+
   private
 
   def set_profile
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :relationships, only: [:create, :destroy]
   resources :participants, only: [:create, :destroy]
   resources :events
   namespace :api, { format: 'json' } do
@@ -8,7 +9,11 @@ Rails.application.routes.draw do
   get '/posts', to: 'posts#new'
   get '/profiles', to: 'profiles#new'
   resources :posts, only: [:new, :create, :edit, :update, :destroy]
-  resources :profiles, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :profiles, only: [:show, :new, :create, :edit, :update, :destroy] do
+    member do
+      get :following, :followers
+    end
+  end
   devise_for :users
   root to: 'home#top'
 end
